6ecade53a9382ff0837405cb11fe739a2aab3c58
sahilsgovekar <sahilsgovekar@gmail.com>
2022-11-14 12:00:29 +0530

    learned section 18 consisting of node.js

---
+++ Section 18 - Node.js/index.js
@@ -0,0 +1,10 @@
+// const fs = require("fs");
+// // import fs from "fs";
+
+// fs.copyFileSync("text1.txt", "text2.txt");
+
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+console.log("You are "+ superheroes.random());
+console.log("You are fighting with "+ supervillains.random());
