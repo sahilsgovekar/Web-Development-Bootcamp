5baba351e3ffbb854cf1e90de2db9933b14a2f31
sahilsgovekar <sahilsgovekar@gmail.com>
2022-11-27 01:38:34 +0530

    sec 28

--- Section 27 - Mongoose/app.js
+++ Section 27 - Mongoose/app.js
@@ -1,20 +1,61 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test');
 
-const mongoose = require("mongoose");
+const Cat = mongoose.model('Cat', { name: String });
 
-mongoose.connect("mongodb://localhost:27017/personDB");
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
 
-const personScheme = new mongoose.Schema({
-    name : String,
-    age : Integer
-});
 
-const Person = mongoose.model("Person", personScheme);
 
-const person = new Person({
-    name : "Sahil",
-    age : 21
-});
 
-person.save();
+
+
+
+
+
+
+
+
+
+// const mongoose = require("mongoose");
+
+// mongoose.connect("mongodb://localhost/personDB", {useNewUrlParser: true}, () =>
+// {
+//     console.log("connected");
+// });
+
+
+
+
+// const personScheme = new mongoose.Schema({
+//     name : String,
+//     age : Number
+// });
+
+// const Person = mongoose.model("Person", personScheme);
+
+
+
+// const person = new Person({
+//     name : "sahil",
+//     age : 21
+// });
+
+// person.save();
+
+// Person.find(function(err, persons)
+// {
+//     if(err)
+//     {    console.log(err);
+//     }
+//     else{
+//         mongoose.connection.close();
+//         console.log(fruits);
+//     }
+// });
+
+
+
 
 
